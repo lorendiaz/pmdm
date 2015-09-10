447e7eaf7574a878a4c4ad0f8a960e291521ecd2
lorendiaz <ldiaz@fpmislata.com>
2015-09-10 12:47:57 +0200

    first commit

---
+++ tema2/Aplicacion2/app/src/main/java/com/fpmislata/practicas/aplicacion2/AcercaDe.java
@@ -0,0 +1,13 @@
+package com.fpmislata.practicas.aplicacion2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcercaDe extends Activity {
+    /** Se ejecuta cuando la actividad es creada por primera vez */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
