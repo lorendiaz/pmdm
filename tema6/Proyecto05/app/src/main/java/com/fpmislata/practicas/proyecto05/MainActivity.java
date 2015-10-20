7c1d9c78a62ea3ef163a1c1bff74163724c3f136
lorendiaz <ldiaz@fpmislata.com>
2015-10-21 00:52:21 +0200

    first commit

---
+++ tema6/Proyecto05/app/src/main/java/com/fpmislata/practicas/proyecto05/MainActivity.java
@@ -0,0 +1,37 @@
+package com.fpmislata.practicas.proyecto05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] paises = { "Argentina", "Chile", "Paraguay", "Bolivia",
+            "Peru", "Ecuador", "Brasil", "Colombia", "Venezuela", "Uruguay" };
+    private String[] habitantes = { "40000000", "17000000", "6500000",
+            "10000000", "30000000", "14000000", "183000000", "44000000",
+            "29000000", "3500000" };
+    private TextView tv1;
+    private ListView lv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1=(TextView)findViewById(R.id.tv1);
+        lv1 =(ListView)findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.expandable_list_content, paises);
+        lv1.setAdapter(adapter);
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int posicion, long id) {
+                tv1.setText("Población de " + lv1.getItemAtPosition(posicion) + " es " + habitantes[posicion]);
+            }
+        });
+    }
+}
