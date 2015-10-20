5c982aa5e7483e90041e8be28ee253ba4be20983
lorendiaz <ldiaz@fpmislata.com>
2015-10-21 00:52:34 +0200

    first commit

---
+++ tema6/Proyecto32/app/src/main/java/com/fpmislata/practicas/proyecto32/MainActivity.java
@@ -0,0 +1,41 @@
+package com.fpmislata.practicas.proyecto32;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle("Importante");
+        dialogo1.setMessage("¿ Acepta la ejecución de este programa en modo prueba ?");
+        dialogo1.setCancelable(false);
+        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                aceptar();
+            }
+        });
+        dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                cancelar();
+            }
+        });
+        dialogo1.show();
+    }
+
+    public void aceptar() {
+        Toast t=Toast.makeText(null,"Bienvenido a probar el programa.", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void cancelar() {
+        finish();
+    }
+}
