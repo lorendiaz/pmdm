8bcc80fa1a5d520b83c28bd68cf8caf4aa4b20a4
lorendiaz <ldiaz@fpmislata.com>
2015-10-31 19:20:01 +0100

    Version 1

---
+++ tema7/Aplicacion29/app/src/main/java/com/fpmislata/practicas/aplicacion29/MainActivity.java
@@ -0,0 +1,24 @@
+package com.fpmislata.practicas.aplicacion29;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Alternativa 1
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //Alternativa 2
+
+        return true;
+    }
+}
